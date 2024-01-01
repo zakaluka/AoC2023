0be479e340e3170ec3b6e3606f9c3f202a0c12fb
zakaluka <znprojects.apps@gmail.com>
2024-01-01 00:16:19 +0000

    Halfway, weird error

--- input3.fs
+++ input3.fs
@@ -1,6 +1,11 @@
 module Input3
 
-let input3a1 = """
+open FSharp.Collections
+open System
+
+let input3a1 =
+  let s =
+    """
 467..114..
 ...*......
 ..35..633.
@@ -12,3 +17,17 @@ let input3a1 = """
 ...$.*....
 .664.598..
 """
+
+  let ses =
+    s.Split [| '\n' |] |> Array.filter (fun l -> String.IsNullOrEmpty(l) |> not)
+
+  let rows = Array.length ses
+  let cols = ses |> Array.map (String.length) |> Array.max
+
+  let arr = Array2D.create rows cols '.'
+
+  for r in 0 .. (Array2D.length1 arr - 1) do
+    let sr = ses[r] |> seq |> Array.ofSeq
+
+    for c in 0 .. (Array2D.length2 arr - 1) do
+      arr[r, c] <- sr[c]
--- logic3.fs
+++ logic3.fs
@@ -2,39 +2,63 @@ module Logic3
 
 open System
 open Input3
+open FSharp.Collections
+open System
+
+let tee f x =
+  f x
+  x
 
 // Some notes:
 // For a 2D array, length1 is the number of rows
 // length2 is the number of columns
-let problem3a = 
-  let isSymbol c = 
-    '.' :: ['0'..'9']
-    |> List.contains c
-    |> not
-    
-  let checkAround arr (r,c) = 
-    let hasSymbolAbove arr (r,c) = 
-      if r = 0 then false 
+let problem3a =
+  let isSymbol c =
+    '.' :: [ '0' .. '9' ] |> List.contains c |> not
+
+  let checkAround (arr:char array2d) (r, c) =
+    let hasSymbolAbove arr (r, c) =
+      if r = 0 then
+        false
       else
-        let above = arr[r - 1,c] |> isSymbol
-        let tl = if c == 0 then false else (arr[r-1,c-1]|>isSymbol)
-        let tr= if c== (Array2D.length2 arr - 1) then false else (arr[r-1,c+1]|>isSymbol)
+        let above = arr[r - 1, c] |> isSymbol
+        let tl = if c = 0 then false else (arr[r - 1, c - 1] |> isSymbol)
+
+        let tr =
+          if c = ((Array2D.length2 arr) - 1) then
+            false
+          else
+            (arr[r - 1, c + 1] |> isSymbol)
+
         tl || above || tr
 
-    let hasSymbolLeft arr (r,c) = 
-      if c == 0 then false else arr[r,c-1]|>isSymbol
+    let hasSymbolLeft arr (r, c) =
+      if c = 0 then false else arr[r, c - 1] |> isSymbol
 
-    let hasSymbolRight arr (r,c) = 
-      if c== (Array2D.length2 arr - 1) then false else (arr[r,c+1]|>isSymbol)
-
-    let hasSymbolBelow arr (r,c) = 
-      if r == (Array@d.length1 arr - 1) then false 
+    let hasSymbolRight arr (r, c) =
+      if c = (Array2D.length2 arr - 1) then
+        false
       else
-        let below = arr[r + 1,c] |> isSymbol
-        let bl = if c == 0 then false else (arr[r+1,c-1]|>isSymbol)
-        let br= if c== (Array2D.length2 arr - 1) then false else (arr[r+1,c+1]|>isSymbol)
+        arr[r, c + 1] |> isSymbol
+
+    let hasSymbolBelow arr (r, c) =
+      if r = (Array2D.length1 arr - 1) then
+        false
+      else
+        let below = arr[r + 1, c] |> isSymbol
+        let bl = if c = 0 then false else (arr[r + 1, c - 1] |> isSymbol)
+
+        let br =
+          if c = (Array2D.length2 arr - 1) then
+            false
+          else
+            arr[r + 1, c + 1] |> isSymbol
+
         bl || above || br
-  
-    (hasSymbolAbove arr (r,c)) || (hasSymbolBelow arr (r,c)) || (hasSymbolLeft arr (r,c)) || (hasSymbolRight arr (r,c))
+
+    (hasSymbolAbove arr (r, c))
+    || (hasSymbolBelow arr (r, c))
+    || (hasSymbolLeft arr (r, c))
+    || (hasSymbolRight arr (r, c))
 
   failwith "Not implemented"
